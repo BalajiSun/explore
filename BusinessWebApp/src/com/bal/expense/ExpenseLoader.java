7fcd4dabbfc604f598d5b4e73370c6d7ad8302b3
Balaji Sundaram <Balaji.Sundaram1983@gmail.com>
2016-12-12 13:24:45 +0000

    System out change

--- BusinessWebApp/src/com/bal/expense/ExpenseLoader.java
+++ BusinessWebApp/src/com/bal/expense/ExpenseLoader.java
@@ -3,7 +3,7 @@ package com.bal.expense;
 public class ExpenseLoader {
 
 	public static void main(String[] args) {
-		System.out.println("First loader");
+		System.out.println("First loader test");
 
 	}
 
